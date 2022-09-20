26ea31cd67ebbd18ad702d8951dff532ba5b0b68
desire barine <desirebarine16@gmail.com>
2022-09-20 20:37:52 +0000

    last task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# configuring SSH client using puppet
+include stdlib
+
+file_line { 'password config OFF':
+ensure  => present,
+path    => '/etc/ssh/ssh_config',
+line    => 'PasswordAuthentication no',
+replace => true,
+}
+file_line { 'Present auth file':
+ensure  => present,
+path    => '/etc/ssh/ssh_config',
+line    => 'IdentityFile ~/.ssh/school',
+replace => true,
+}
